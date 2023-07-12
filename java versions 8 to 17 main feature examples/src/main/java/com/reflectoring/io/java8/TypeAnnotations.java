46da48c946dccbf70771f48ea3725186f4961ddb
Tushar Agrawal <tushar.agrawal@aurigait.com>
2023-07-12 12:03:22 +0530

    first commit

---
+++ java versions 8 to 17 main feature examples/src/main/java/com/reflectoring/io/java8/TypeAnnotations.java
@@ -0,0 +1,27 @@
+package com.reflectoring.io.java8;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+public class TypeAnnotations {
+
+    public static void main(String[] args) {
+
+        @NotNull String userName = args[0];
+
+        List<String> request =
+                new @NotEmpty ArrayList<>(Arrays.stream(args).collect(
+                        Collectors.toList()));
+
+        List<@Email String> emails;
+
+
+    }
+}
